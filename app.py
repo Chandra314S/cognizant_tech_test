acbd6f6da5b8a2a60e329ee8c77a384e336e657a
Chandra Sekhar Jampula <chandrajampula@gmail.com>
2022-07-08 15:52:28 +0100

    Upadting docker file and app port numbers

--- app.py
+++ app.py
@@ -123,4 +123,5 @@ def UploadData():
     return(redirect(url_for("APP_root")))
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    # app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
