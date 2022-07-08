b397670713dd6fc94d404fe71efe04efc78ecbbd
Chandra Sekhar Jampula <chandrajampula@gmail.com>
2022-07-08 16:50:25 +0100

    Intialize cicd pipeline

--- app.py
+++ app.py
@@ -122,6 +122,7 @@ def UploadData():
 
     return(redirect(url_for("APP_root")))
 
+#This is to find out cicd status
 if __name__ == "__main__":
     # app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
